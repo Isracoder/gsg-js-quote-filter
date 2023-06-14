c96d6d1a743e36061cbcec680583125c03fcd1ad
Isracoder <90979049+Isracoder@users.noreply.github.com>
2023-06-14 09:43:00 +0300

    Update filterQuotes.js
    getting rid of comma between list items

--- filterQuotes.js
+++ filterQuotes.js
@@ -2,68 +2,69 @@ let div = document.querySelector(".box");
 let quotes = [];
 let arr = [];
 getQuotes();
-let apost = '’' ;
+let apost = "’";
 function main() {
-  
-  if (typeof document !== "undefined") {
-    
-    if (Object.keys(quotes).length == 0) {
-      console.log("uh oh , an error has occured");
-      div.innerHTML =
-        "We are currently unable to display any quotes at the moment.";
-    } else {
-      let form = document.getElementById("form");
-      let input = document.querySelector(".input");
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let value = input.value;
-        // the apostrophe used in the quotes is different from the keyboard apostrophe
-        let newArr = [];
-        quotes["quotes"].forEach((obj) => {
-          if (obj.quote.toLowerCase().includes(value.toLowerCase()) || obj.quote.toLowerCase().includes(value.replace(/\'/g , apost))) {
-            newArr.push(obj.quote + "- " + obj.author);
-          }
-        });
-        onSuccess(newArr);
-      });
-    }
-  }
+	if (typeof document !== "undefined") {
+		if (Object.keys(quotes).length == 0) {
+			console.log("uh oh , an error has occured");
+			div.innerHTML =
+				"We are currently unable to display any quotes at the moment.";
+		} else {
+			let form = document.getElementById("form");
+			let input = document.querySelector(".input");
+			form.addEventListener("submit", (e) => {
+				e.preventDefault();
+				let value = input.value;
+				// the apostrophe
+				let newArr = [];
+				quotes["quotes"].forEach((obj) => {
+					if (
+						obj.quote.toLowerCase().includes(value.toLowerCase()) ||
+						obj.quote
+							.toLowerCase()
+							.includes(value.replace(/\'/g, apost))
+					) {
+						newArr.push(obj.quote + "- " + obj.author);
+					}
+				});
+				onSuccess(newArr);
+			});
+		}
+	}
 }
-// can be improved with async await syntax , however it's currently functioning without it 
+// can be improved with async await syntax , however it's currently functioning without it
 function getQuotes() {
-  fetch("https://dummyjson.com/quotes", { method: "GET" })
-    .then((res) => res.json())
-    .then((ans) => {
-      console.log(`ans ${ans.quotes[0]}`);
-      quotes = ans;
-      arr = cleanData(arr);
-      onSuccess(arr);
-      
-      main();
-    })
-    .catch((err) => {
-      console.log(`a ${err} has occured`);
-      div.innerHTML =
-        "We are currently unable to display any quotes at the moment.";
-    
-    });
+	fetch("https://dummyjson.com/quotes", { method: "GET" })
+		.then((res) => res.json())
+		.then((ans) => {
+			console.log(`ans ${ans.quotes[0]}`);
+			quotes = ans;
+			arr = cleanData(arr);
+			onSuccess(arr);
+
+			main();
+		})
+		.catch((err) => {
+			console.log(`a ${err} has occured`);
+			div.innerHTML =
+				"We are currently unable to display any quotes at the moment.";
+		});
 }
 function cleanData(arr) {
-  for (let i = 0; i < quotes["quotes"].length; i++) {
-    let str = quotes["quotes"][i].quote + " -" + quotes["quotes"][i].author;
-    if (str[str.length - 1] == ",") console.log("comma");
-    arr.push(str);
-    
-  }
-  return arr;
+	for (let i = 0; i < quotes["quotes"].length; i++) {
+		let str = quotes["quotes"][i].quote + " -" + quotes["quotes"][i].author;
+		if (str[str.length - 1] == ",") console.log("comma");
+		arr.push(str);
+	}
+	return arr;
 }
 function onSuccess(arr) {
-  if (arr.length == 0) {
-    div.innerHTML = "Sorry , no quotes contain that search";
-    return;
-  }
-  let str = `<ul>
-          ${arr.map((elem) => `<li>${elem}</li>`)}
+	if (arr.length == 0) {
+		div.innerHTML = "Sorry , no quotes contain that search";
+		return;
+	}
+	let str = `<ul>
+          ${arr.map((elem) => `<li>${elem}</li>`).join("")}
         </ul>`;
-  div.innerHTML = str;
+	div.innerHTML = str;
 }
